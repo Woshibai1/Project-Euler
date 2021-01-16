6bf95c2194d17bed1c4c859882c31560633b9433
Woshibai1 <70039513+Woshibai1@users.noreply.github.com>
2021-01-16 16:04:03 +0300

    Delete 9.py

--- 9.py
+++
@@ -1,7 +0,0 @@
-for a in range (1, 1000):
-    for b in range (1, 1000):
-        for c in range (1, 1000):
-            if  c**2 == a**2+b**2:
-                if a+b+c == 1000 and a<b<c:
-                    print (a*b*c)
-    
