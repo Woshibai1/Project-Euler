7ecea86a2178f62c2cae9792be277d91d940c470
Woshibai1 <70039513+Woshibai1@users.noreply.github.com>
2021-09-24 12:08:05 +0300

    Update 9.py

--- 9.py
+++ 9.py
@@ -4,6 +4,6 @@ def pythagoreanTriplet(n):
             for c in range (1, 1000):
                 if  c**2 == a**2+b**2:
                     if a+b+c == n and a<b<c:
-                        print (a*b*c)
+                        return (a*b*c)
               
 print (pythagoreanTriplet(1000))
